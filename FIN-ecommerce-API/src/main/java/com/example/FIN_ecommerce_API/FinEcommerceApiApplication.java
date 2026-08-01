a362ac9d9fd5af08d121269d800c9e985e64a0b5
phynaphy <phyna59@gmail.com>
2026-08-01 17:06:53 +0700

    Initial commit

---
+++ FIN-ecommerce-API/src/main/java/com/example/FIN_ecommerce_API/FinEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FIN_ecommerce_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinEcommerceApiApplication.class, args);
+	}
+
+}
---
+++ FIN-ecommerce-API/src/test/java/com/example/FIN_ecommerce_API/FinEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FIN_ecommerce_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinEcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
